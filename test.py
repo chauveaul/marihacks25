81bd68325e28640a5022ee9de461b9e03b750931
MeZe <lucaschauveau007@gmail.com>
2025-04-04 19:13:08 -0400

    Initial commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+data = np.zeros((512, 512, 3), dtype=np.uint8)
+data[256, 256] = [255, 0, 0]
+
+plt.imshow(data, interpolation="nearest")
+plt.show()
